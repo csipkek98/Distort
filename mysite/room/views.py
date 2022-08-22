ab35fa369f55b97b9e096073cd110668e0dfc26f
SentexX <csipkekrisztian@gmail.com>
2022-08-22 20:31:51 +0200

    Add: logic in view.py to show the last 25 message

--- mysite/room/views.py
+++ mysite/room/views.py
@@ -12,6 +12,12 @@ def rooms(request):
 @login_required
 def room(request, slug):
     room = Room.objects.get(slug=slug)
-    messages = Message.objects.filter(room=room)[0:25]
+    messages_raw = Message.objects.filter(room=room)
+    messages_len = len(messages_raw)
+    load_in_message_size = 25
+    if(messages_len <= load_in_message_size):
+        messages = messages_raw
+    else:
+        messages = messages_raw[(messages_len-load_in_message_size-1):(messages_len)]
 
     return render(request, 'room/room.html', {'room': room, 'messages': messages})
