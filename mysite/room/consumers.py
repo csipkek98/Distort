7e436ee376414ef444402c67e49fad29d4a02b7c
SentexX <csipkekrisztian@gmail.com>
2022-08-26 00:28:27 +0200

    Add: feature to manage typing users in the backend

--- mysite/room/consumers.py
+++ mysite/room/consumers.py
@@ -11,6 +11,13 @@ from .models import Message, Room
 class ChatConsumer(AsyncWebsocketConsumer):
 
     __userlist = dict()
+    __typing_userlist = dict()
+
+    async def getTypingList(self):
+        return self.__typing_userlist
+    
+    async def setTypingList(self, typing_list):
+        self.__typing_userlist = typing_list
 
     async def getUserList(self):
         return self.__userlist
@@ -64,9 +71,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
                     'user_list':user_list[room],
                 }
             )
-        else:
+        elif 'message' in data:
             message = data['message']
-            room = data['room']
             role = data['super_user']
 
             await self.save_message(username,room,message)
@@ -81,6 +87,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
                     'super_user': role,
                 }
         )
+        elif 'typing' in data:
+            typing = data['typing']
+            typing_list = await self.getTypingList()
+            if(room not in typing_list):
+                    typing_list[room] = set()
+            if(typing):
+                typing_list[room].add(username)
+            else:
+                print(typing_list[room])
+                typing_list[room].remove(username)
+                print(f"User {username} stopped Typing!")
+            await self.setTypingList(typing_list)
+            print(await self.getTypingList())
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'actual_typing_list',
+                    'typing_list':typing_list[room],
+                }
+            )
+        else:
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~\nNOT HANDLED RECEIVE METHOD\nPLEASE WRITE ONE! :)\n~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
     async def take_usernames(self, event):
         boolean = event['build_user_in_room_list']
@@ -107,6 +135,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.send(text_data=json.dumps({
             'user_list': user_list,
+        }))
+
+    async def actual_typing_list(self,event):
+        typing_list = ",".join(event['typing_list'])
+
+        await self.send(text_data=json.dumps({
+            'typing_list': typing_list,
         }))
 
     @sync_to_async
