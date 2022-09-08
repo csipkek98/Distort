62e826ae1b385604a019eb8ce21f9c4f53fe913b
SentexX <csipkekrisztian@gmail.com>
2022-09-08 23:08:59 +0200

    Add: profile/ route for base url pattern searching

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rooms/', include("room.urls")),
+    path('profile/', include('userprofile.urls')),
     path('', include("core.urls")),
 ]
 
