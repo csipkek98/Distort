ab02cd74830598d1dc945e8dc12671a2397ec7a0
SentexX <csipkekrisztian@gmail.com>
2022-09-22 23:10:52 +0200

    Add: password reset templates added for done, complete and confirm

--- mysite/core/urls.py
+++ mysite/core/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password/', views.change_password, name='change_password'),
     path('password_reset/', views.password_reset_request, name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='core/password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="core/password/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='core/password/password_reset_complete.html'), name='password_reset_complete'),
 ]
