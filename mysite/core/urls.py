0d5a6295d8e5e3fb691795933506509883f0174f
SentexX <csipkekrisztian@gmail.com>
2022-09-22 18:05:59 +0200

    Add: new routes to reset password in core urls.py

--- mysite/core/urls.py
+++ mysite/core/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password/', views.change_password, name='change_password'),
+    path('password_reset/', views.password_reset_request, name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
