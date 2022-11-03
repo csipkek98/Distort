ad1839d5cecc25340f4d805dc6e2fef5aa9cfa62
SentexX <csipkekrisztian@gmail.com>
2022-11-03 13:14:40 +0100

    Add: import the necessary modules to views.py

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -12,6 +12,9 @@ from django.db.models.query_utils import Q
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
+from django.conf import settings
+
+from logger import *
 
 from .forms import SignUpForm
 
